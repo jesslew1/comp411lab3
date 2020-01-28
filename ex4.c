5e0f841d0207f2c03b2ac729e437b853d5e08b07
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-27 20:29:18 -0500

    Add files via upload

--- ex4.c
+++ ex4.c
@@ -27,7 +27,7 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
 			C[i][j] = A[i][j] + B[i][j];
-			printf("%*d", C[i][j], 10);
+			printf("%*d", 10, C[i][j]);
 		}
 		printf("\n");
 	}
