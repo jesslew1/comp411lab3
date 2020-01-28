ffc1024a9a714df8d7704138f491e32aea3bd29c
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-28 16:38:21 -0500

    Add files via upload

--- ex3.c
+++ ex3.c
@@ -47,14 +47,14 @@ length2 = strlen(text2) - 1;
 
 //   digits = 0;
   for (int i = 0; i < length2; i++){
-    if (reverse2[i] != text2[i]){
-        digits = digits +1;
-        // break;
+    if (reverse2[i] == text2[i]){
+        digits = digits +0;
+        continue;
     } else {
-        digits = digits + 0;
+        digits = digits + 1;
     }
   }
-  if (digits <= 0){
+  if (digits == 0){
       printf("Found a palindrome!\n");
   }
 
