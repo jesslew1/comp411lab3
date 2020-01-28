4a5596179a0ef517fce8d623a9ab7692537f65c8
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-28 16:26:25 -0500

    Add files via upload

--- ex3.c
+++ ex3.c
@@ -24,21 +24,21 @@ int main()
   reverse[revLength] = '\0';
   printf("Your input in reverse is:\n%s\n", reverse);
 
-int j = -1;
+int j = 0;
 for (int i = 0; i <= length; i++){
     // int j = 0;
     if (isspace(text[i]) || !isalpha(text[i]) || ispunct(text[i])){
         continue;
     } if (!isspace(text[i]) && isalpha(text[i]) && !ispunct(text[i])){
-        j++;
-        text2[j] = text[i];
         // j++;
+        text2[j] = text[i];
+        j++;
     }
 }
 length2 = strlen(text2) - 1;
-// for (int i =0; i < (length2+1); i ++){
-//     text2[i] = tolower(text2[i]);
-// }
+for (int i =0; i < (length2+1); i ++){
+    text2[i] = tolower(text2[i]);
+}
  for (int i = 0; i <= length2; i++){
       reverse2[i] = text2[(length2 - 1) - i];
   }
