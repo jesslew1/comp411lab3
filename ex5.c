dfdf5f31f3660a96f477df13032da96511fa8217
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-27 20:39:29 -0500

    Add files via upload

---
+++ ex5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_BUF 1024
+
+int main () {
+
+  char buf[MAX_BUF];
+  int length;
+  // other stuff
+
+  do {
+       // read a line
+       fgets(buf, MAX_BUF, stdin);
+        // calculate its length
+        length = strlen(buf) - 1;
+       // modify the line by switching characters
+       if (length ==0){
+           break;
+       }
+      for (int i = 0; i < MAX_BUF; i++){
+          charc = buf[i];
+          if (charc == 'I'|| charc == 'i')
+				buf[i] = '1';
+          if (charc == 'S'|| charc == 's')
+				buf[i] = '5';
+          if (charc == 'E'|| charc == 'e')
+				buf[i] = '3';
+		  if (charc == 'O'|| charc == 'o')
+				buf[i] = '0';
+		}
+      }
+        // print the modified line
+      printf("%s", buf);
+  } while (length > 1);
+
+}
