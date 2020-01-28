bda0a72d47d68a49ffc48205b12c54973611d9f1
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-27 19:11:45 -0500

    Add files via upload

--- ex2.c
+++ ex2.c
@@ -16,21 +16,21 @@ int main()
 
   fgets(text, MAX, stdin);
   length = strlen(text) - 1;
-
-  for (int i = 0; i <= length; i++){
+ for (int i = 0; i <= length; i++){
       reverse[i] = text[(length - 1) - i];
   }
   revLength = strlen(text);
   reverse[revLength] = '\0';
   printf("Your input in reverse is:\n%s\n", reverse);
   digits = 0;
-  for (int i = 0; i <= length; i++){
+  for (int i = 0; i < length; i++){
+//      digits =0;
       if (text[i] != reverse[i]){
-          digits = 1;
-          break;
-      } else {
-          digits = 0;
-      }
+         digits = 1;
+        break; 
+     } else {
+        digits = 0;
+        }
   }
   if (digits == 0){
       printf("Found a palindrome!\n");
