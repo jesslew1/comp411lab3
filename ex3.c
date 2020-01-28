de9769058996087586dfa67f40c8bc48a9765c53
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-28 15:12:36 -0500

    Add files via upload

--- ex3.c
+++ ex3.c
@@ -1,130 +1,67 @@
-// /* Example: analysis of text */
-
-// #include <stdio.h>
-// #include <string.h>
-// #include <ctype.h>
-
-// #define MAX 1000 /* The maximum number of characters in a line of input */
-
-// int main()
-// {
-//   char text[MAX], reverse[MAX], text2[MAX], reverse2[MAX];
-//   int i;
-//   int lowercase, uppercase, digits, other;
-//   int length, revLength;
-  
-//   puts("Type some text (then ENTER):");
-
-//   fgets(text, MAX, stdin);
-//   length = strlen(text) - 1;
-//   for (int i = 0; i <= length; i++){
-//       reverse[i] = text[(length - 1) - i];
-//   }
-//   revLength = length + 1;
-//   reverse[revLength] = '\0';
-//   printf("Your input in reverse is:\n%s\n", reverse);
-
-// // for (int i =0; i < length; i++){
-// //     if (!isalpha(text[i])){
-// //         break;
-// //     } else {
-// //         toupper(text[i]) == tolower(text[i]);
-// //     }
-// // }
-// for (int i = 0; i <= length; i++){
-//     if (isspace(text[i]) || !isalpha(text[i]) || ispunct(text[i])){
-//         continue;
-//     } if 
-// }
-
-//   digits = 0;
-//   for (int i = 0; i < length; i++){
-//     if (text2[i] != reverse2[i]){
-//           digits = 1;
-//         break; 
-//      } else {
-//         digits = 0;
-//         }
-//   }
-//   if (digits == 0){
-//       printf("Found a palindrome!\n");
-//   }
-
-
-// }
+/* Example: analysis of text */
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-#define MAX 1000 /*The maximum number of characters in a line of input */
+#define MAX 1000 /* The maximum number of characters in a line of input */
 
-int main(){
-	char text[MAX], reverse[MAX], newtext[MAX], newrev[MAX];
-	int i, nsp;
-	int digits, other;
-	int length, count, rlength;
+int main()
+{
+  char text[MAX], reverse[MAX], text2[MAX], reverse2[MAX];
+  int i;
+  int lowercase, uppercase, digits, other;
+  int length, revLength;
+  
+  puts("Type some text (then ENTER):");
 
-	puts("Type some text (then ENTER):");
+  fgets(text, MAX, stdin);
+  length = strlen(text) - 1;
+  for (int i = 0; i <= length; i++){
+      reverse[i] = text[(length - 1) - i];
+  }
+  revLength = length + 1;
+  reverse[revLength] = '\0';
+  printf("Your input in reverse is:\n%s\n", reverse);
 
-	/*Save typed characters in text[]: */
+// for (int i =0; i < length; i++){
+//     if (!isalpha(text[i])){
+//         break;
+//     } else {
+//         toupper(text[i]) == tolower(text[i]);
+//     }
+// }
+for (int i = 0; i <= length; i++){
+    int j = 0;
+    if (isspace(text[i]) || !isalpha(text[i]) || ispunct(text[i])){
+        continue;
+    } if (!isspace(text[i]) && isalpha(text[i]) && !ispunct(text[i])){
+        text2[j] = text[i];
+        j++;
+    }
+}
+length = strlen(text2) -1;
+for (int 1 =0; i < (length+1); i ++){
+    text2[i] = toupper(text2[i]);
+}
+ for (int i = 0; i <= length; i++){
+      reverse2[i] = text2[(length - 1) - i];
+  }
+revLength = length + 1;
+  reverse2[revLength] = '\0';
 
-	fgets(text, MAX, stdin);
-	
-	length = strlen(text) - 1;
-	count = length - 1;
-
-	/* reverse the contents of text[]: */
-	for (i = 0; i <= length; i++) {
-		reverse[i] = text[count - i];
-	}
-	rlength = strlen(text);
-	reverse[rlength] = '\0';
-
-	printf("Your input in reverse is:\n");
-	printf("%s\n", reverse);
-	
-	nsp = -1;
-	
-	/* checks for spaces, punctuation, or numbers in the original array*/
-	for (i = 0; i <= length; i++) {
-		if (isspace(text[i]) || ispunct(text[i]) || !isalpha(text[i])) {
-			continue;
-		}
-		if (!isspace(text[i]) && !ispunct(text[i]) && isalpha(text[i])) {
-			nsp++;
-			newtext[nsp] = text[i];
-		} 
-	}
+  digits = 0;
+  for (int i = 0; i < length; i++){
+    if (text2[i] != reverse2[i]){
+          digits = 1;
+        break; 
+     } else {
+        digits = 0;
+        }
+  }
+  if (digits == 0){
+      printf("Found a palindrome!\n");
+  }
 
 
-	/*changes all uppercase letters to lowercase*/
-	for (i = 0; i <= strlen(newtext); i++) {
-		newtext[i] = tolower(newtext[i]);
-	}
-
-	digits = 0;
-	
-	length = strlen(newtext) - 1;
-	count = length - 1;
-	/*reverse the contents of newtext[]:*/
-	for (i = 0; i <= length; i++) {
-		newrev[i] = newtext[length - i];
-	}
-
-	rlength = strlen(newtext);
-	newrev[rlength] = '\0';
-
-	for (i = 0; i < length; i++) {
-		 if (newrev[i] == newtext[i]) {	
-			 continue;
-		} else {
-			digits = 1;
-		}
-		
-	}
-
-	if (digits == 0) {
-		printf("Found a palindrome!\n");
-	}
 }
