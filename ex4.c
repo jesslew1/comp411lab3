a9f65744a1dfc5373ecc0fcf6b3c4dc35bc5c367
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-27 20:17:17 -0500

    Add files via upload

--- ex4.c
+++ ex4.c
@@ -7,7 +7,7 @@ int main()
   int A[3][3];    // matrix A     
   int B[3][3];    // matrix B
   int C[3][3];    // matrix to store their sum
-
+int sum; 
   // add your code below
   printf("Please enter 9 values for matrix A:\n");
   	for (int i = 0; i < 3; i++) {
@@ -22,7 +22,7 @@ int main()
 			scanf("%d", &B[i][j]);
 		}
 	}
-   for (i = 0; i < 3; ++i){
+   for (int i = 0; i < 3; ++i){
         for (int j = 0; j < 3; ++j) {
             sum[i][j] = &A[i][j] + &B[i][j];
         }
