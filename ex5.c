fc47c155625dfe1189111961998d14dbf6330125
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-28 12:42:23 -0500

    this works

--- ex5.c
+++ ex5.c
@@ -17,14 +17,14 @@ int main () {
        // modify the line by switching characters
       for (int i = 0; i < MAX_BUF; i++){
           charc = buf[i];
+		      if (charc == 'O'|| charc == 'o')
+			      	buf[i] = '0';
           if (charc == 'I'|| charc == 'i')
 				      buf[i] = '1';
-          if (charc == 'S'|| charc == 's')
-			      	buf[i] = '5';
           if (charc == 'E'|| charc == 'e')
 				      buf[i] = '3';
-		  if (charc == 'O'|| charc == 'o')
-			      	buf[i] = '0';
+          if (charc == 'S'|| charc == 's')
+			      	buf[i] = '5';
 		}
         // print the modified line
       printf("%s", buf);
