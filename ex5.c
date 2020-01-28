de4afbf36e00dec35a782f98ccf0caaad7797791
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-28 12:37:34 -0500

    Add files via upload

--- ex5.c
+++ ex5.c
@@ -13,18 +13,18 @@ int main () {
         // calculate its length
         length = strlen(buf) - 1;
        // modify the line by switching characters
-       if (length ==0)
-           break;
+       if (length != 0)
+           continue;
       for (int i = 0; i < MAX_BUF; i++){
           charc = buf[i];
           if (charc == 'I'|| charc == 'i')
-				buf[i] = '1';
+				      buf[i] = '1';
           if (charc == 'S'|| charc == 's')
-				buf[i] = '5';
+			      	buf[i] = '5';
           if (charc == 'E'|| charc == 'e')
-				buf[i] = '3';
+				      buf[i] = '3';
 		  if (charc == 'O'|| charc == 'o')
-				buf[i] = '0';
+			      	buf[i] = '0';
 		}
         // print the modified line
       printf("%s", buf);
