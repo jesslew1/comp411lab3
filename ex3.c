943dc6bc8382b5a2a91ffeb0b699d84e5e9a9b3d
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-28 16:23:29 -0500

    Add files via upload

--- ex3.c
+++ ex3.c
@@ -48,10 +48,10 @@ reverse2[revLength2] = '\0';
   digits = 0;
   for (int i = 0; i < length2; i++){
     if (text2[i] == reverse2[i]){
-        digits = digits + 0;
+        digits = 0;
         // break;
     } else {
-        digits = digits + 1;
+        digits = 1;
         break;
     }
   }
