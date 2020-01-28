60ff155bd9db45944e98beb71182f4128070f30b
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-27 20:11:01 -0500

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,32 @@
+/* Example: matrices represented by 2-dimensional arrays */
+
+#include <stdio.h>
+
+int main()
+{
+  int A[3][3];    // matrix A     
+  int B[3][3];    // matrix B
+  int C[3][3];    // matrix to store their sum
+
+  // add your code below
+  printf("Please enter 9 values for matrix A:\n");
+  	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+				scanf("%d", &A[i][j]);
+		}
+	}
+	printf("Please enter 9 values for matrix B:\n");
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			scanf("%d", &B[i][j]);
+		}
+	}
+   for (i = 0; i < r; ++i){
+        for (j = 0; j < c; ++j) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+  
+
+}
