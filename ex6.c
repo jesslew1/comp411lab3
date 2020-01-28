475cb03536b64319a98984e8851c61168117ef2f
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-01-27 20:51:59 -0500

    Add files via upload

--- ex6.c
+++ ex6.c
@@ -13,7 +13,7 @@ int main() {
 	int n, k, nCk;
 		printf("Enter two integers (for n and k) separated by space:\n");
 		scanf("%d%d", &n, &k);
-        while (n > 0 && k > 0){
+        while (true){
             if (n == 0 && k == 0){
                 break;
             } else {
